54d70e207e1f0330ba48f0cafeaf8dca0ddfea93
Lazytangent <pm123633@gmail.com>
2021-06-29 10:19:22 -0500

    Add store files

---
+++ frontend/src/store/articleReducer.js
@@ -0,0 +1,30 @@
+import articles from '../data/data.json';
+
+const GET_ARTICLES = 'article/getArticles';
+const ADD_ARTICLE = 'article/addArticle';
+
+export const getArticles = () => {
+  return { type: GET_ARTICLES, articles };
+};
+
+export const addArticle = (newArticle) => {
+  return {
+    type: ADD_ARTICLE,
+    newArticle,
+  };
+};
+
+const initialState = { articles: [], isLoading: true };
+
+const articleReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_ARTICLES:
+      return { ...state, articles: [...action.articles] };
+    case ADD_ARTICLE:
+      return { ...state, articles: [...state.articles, action.newArticle] };
+    default:
+      return state;
+  }
+};
+
+export default articleReducer;
